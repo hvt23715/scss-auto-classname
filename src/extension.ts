07f509de9a272488409b77902b4bc08aac2937a0
黄腾 <huangteng@dazhuanjia.com>
2018-08-21 10:39:42 +0800

    test

--- src/extension.ts
+++ src/extension.ts
@@ -14,41 +14,51 @@ export function activate(context: vscode.ExtensionContext) {
         if (!event) {
             return;
         }
-        if(event.document.languageId !== 'scss') {
+        if(event.document.languageId !== 'scss' && event.document.languageId !== 'javascript') {
             return;
         }
         firstClassName = '';
     });
     // 输入字符的时候
     vscode.workspace.onDidChangeTextDocument(event => {
-        if(event.document.languageId !== 'scss') {
+        if(event.document.languageId !== 'scss' && event.document.languageId !== 'javascript') {
             return;
         }
         // 如果类名为空，获取类明
         if(firstClassName === ''){
-            firstClassName = getFirstClassName();
+            firstClassName = getFirstClassName(event);
         }
+        console.log(firstClassName);
         // 将&换成类名
         changeToCompleteName(event,firstClassName);
     });
 
 }
 
-export function deactivate() {
-}
 
-function getFirstClassName(): string {
+function getFirstClassName(event: vscode.TextDocumentChangeEvent): string {
     let editor = vscode.window.activeTextEditor;
     if (!editor) {
         return '';
     }
+    let pattern = /^/;
+    if(event.document.languageId === 'scss'){
+        pattern = /.?([a-z0-9]+\-?)+[\s]?\{/;
+    }
+    if(event.document.languageId === 'javascript'){
+        pattern = /className=("|')([a-z0-9]+\-?)+/;
+    }
     let classline = 0;
     while(classline < editor.document.lineCount){
         let text = editor.document.lineAt(classline).text;
-        let pattern = /.([a-z0-9]+\-?)+[\s]?\{/;
         let firstClassName = pattern.exec(text);
         if(firstClassName !== null){
-            return firstClassName[0].replace(/[\s]*{/,'');
+            if(event.document.languageId === 'scss'){
+                return firstClassName[0].replace(/[\s]*{/,'');
+            }
+            if(event.document.languageId === 'javascript'){
+                return firstClassName[0].replace(/className=("|')/,'');
+            }
         }
         classline += 1;
     }
@@ -61,7 +71,7 @@ function changeToCompleteName(event: vscode.TextDocumentChangeEvent,firstClassNa
     }
    
     // 如果不是&就直接返回
-    if (event.contentChanges[0].text !== '&'){
+    if (event.contentChanges[0].text !== '&' && event.contentChanges[0].text !== '-'){
         return;
     }
 
@@ -71,25 +81,35 @@ function changeToCompleteName(event: vscode.TextDocumentChangeEvent,firstClassNa
     }
 
 
+
     let selection = editor.selection;
     let originalPosition = selection.start.translate(0, 1);
     let deletePosition = selection.start.translate(0, 0);
     let prevPosition = selection.start.translate(0, -1);
 
-
     // 判断&的前一个字符
     let prevChar = editor.document.getText(new vscode.Range(prevPosition , deletePosition));
+
+
+    let twoChae = prevChar+event.contentChanges[0].text;
+
     // 如果输入-& 就取第一个class，不是就取最近的一个父级元素
-    if(prevChar === '-'){
+    if(twoChae === '-&'){
         editor.edit((editBuilder) => {
             editBuilder.delete(new vscode.Range(prevPosition, originalPosition));
             editBuilder.insert(originalPosition, firstClassName);
         });
-    }else{
+    }else if(twoChae === '&-'){
         let recentClassName = '';
         // 用栈的思想实现
         let content = editor.document.getText(new vscode.Range(new vscode.Position(0, 0) , deletePosition));
-        let classNamePattern = /}|.([a-z0-9]+\-?)+[\s]?\{/;
+        let classNamePattern = /-/;
+        if(event.document.languageId === 'scss'){
+            classNamePattern = /@media screen and \(|}|.?([a-z0-9]+\-?)+[\s]?\{/;
+        }
+        if(event.document.languageId === 'javascript'){
+            classNamePattern = /<\/|\/>|</;
+        }
         // let classEndPattern = /}/;
         let ifGetRecentClassName = false;
         let classNameArr = [];
@@ -107,28 +127,52 @@ function changeToCompleteName(event: vscode.TextDocumentChangeEvent,firstClassNa
             }
 
         }
-        // console.log(classNameArr);
-
+        console.log(classNameArr);
+// --------------------------
         let classEnd = 0;
-
-        for(let i=classNameArr.length-1 ; i>=0 ; i--){
-            if(classNameArr[i] === '}'){
-                classEnd+=1;
+        if(event.document.languageId === 'javascript'){
+            for(let i=classNameArr.length-1 ; i>=0 ; i--){
+                if(classNameArr[i] === '}'){
+                    classEnd+=1;
+                }
+                if(classNameArr[i] !== '}'){
+                    classEnd-=1;
+                }
+                if(classEnd < 0){
+                    if(/@media /.exec(classNameArr[i]) !== null){
+                        classEnd = 0;
+                        continue;
+                    }
+                    recentClassName = classNameArr[i].replace(/(^\s*)|[\s]*{/g,'');
+                    break;
+                }
             }
-            if(classNameArr[i] !== '}'){
-                classEnd-=1;
-            }
-            if(classEnd < 0){
-                recentClassName = classNameArr[i].replace(/[\s]*{/,'');
-                break;
-            }
+        }else if(event.document.languageId === 'scss'){
+            for(let i=classNameArr.length-1 ; i>=0 ; i--){
+                if(classNameArr[i] === '}'){
+                    classEnd+=1;
+                }
+                if(classNameArr[i] !== '}'){
+                    classEnd-=1;
+                }
+                if(classEnd < 0){
+                    if(/@media /.exec(classNameArr[i]) !== null){
+                        classEnd = 0;
+                        continue;
+                    }
+                    recentClassName = classNameArr[i].replace(/(^\s*)|[\s]*{/g,'');
+                    break;
+                }
+            }    
         }
-        // recentClassName = classNameArr[classNameArr.length-classEndArr.length-1].replace(/[\s]*{/,'');
 
         editor.edit((editBuilder) => {
-            editBuilder.delete(new vscode.Range(deletePosition, originalPosition));
+            editBuilder.delete(new vscode.Range(prevPosition, originalPosition));
             editBuilder.insert(originalPosition, recentClassName);
         });
+        return;
+    }else{
+        return;
     }
     
     
